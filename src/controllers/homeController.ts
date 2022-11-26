16164b203b7ab2956694487898b9187c4807c041
Denny Azevedo <denny.filho@animaeducacao.com.br>
2022-11-25 21:21:46 -0300

    Manipulando dados no MongoDB

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -6,7 +6,26 @@ import User from '../models/User';
 export const home = async (req: Request, res: Response) => {
     //let usuarios = await User.find({}); //todos
     //let usuarios = await User.findOne({ email: 'dennyazevedo@gmail.com' }); //unico
-    let usuarios = await User.findById('636f003790976a2cf544c037'); //pelo id
+    //let usuarios = await User.findById('636f003790976a2cf544c037'); //pelo id
+    //let usuarios = await User.find({ "fullName.firstName": "Denny" });
+    //let usuarios = await User.find({ interests: "Programação" });
+    //let usuarios = await User.find({ age: { $gt: 25 } });
+    //let usuarios = await User.find({ age: { $gt: 25, $lt: 45 } });
+    /*
+        gt = Greater Then = maior
+        gte = Greater Then or Equal = Maior ou Igual
+        lt = Lower Then = Menor
+        lte = Lower Then or Equal = Menor ou Igual
+
+        ordenação: 
+            1 - ascendente
+            -1 - descendente
+    */
+    //let usuarios = await User.find({ age: { $gt: 25 } }).sort({ age: -1 });
+    //let usuarios = await User.find({ age: { $gt: 25 } }).sort({ email: 1 });
+    //let usuarios = await User.find({ age: { $gt: 25 } }).sort({ "fullName.firstName": -1 });
+    let usuarios = await User.find({ age: { $gt: 25 } }).sort({ "fullName.firstName": 1, age: -1 });
+    
     console.log("USUARIOS", usuarios);
     
     let age: number = 58;
